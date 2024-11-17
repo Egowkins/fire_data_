3cb961985fa8a402683022fbe65b73ad5694e43d
Egowkins <zabrodkin.eo@edu.spbstu.ru>
2024-11-17 21:46:30 +0300

    ver 0.2 new excel file, new version of cropping images.

--- video_to_frames.py
+++ video_to_frames.py
@@ -3,79 +3,72 @@ import pandas as pd
 import os
 
 
-# Функция для конвертации времени (например, в секундах) в кадры
+# Функция для конвертации времени в кадры
 def time_to_frame(time_in_seconds, fps):
     return int(time_in_seconds * fps)
 
 
-#video_path = 'video/DSC_2365.MOV'  - target_video
-#timing_file = 'regims/regim1.xlsx' - example
-def regims(video_path_:str,  mode_, start_time_, end_time_, c_frame_need: int) -> None:
+def extract_n_frames_per_second(video_path: str, timing_data: pd.DataFrame, n_frames_per_second: int, info: False) -> None:
     """
-
-    :param video_path_: путь к видео
-    #:param timing_file_: файл с таймингами
-    :param mode_: какой режим
-    :param start_time_: начало режима
-    :param end_time_: конец режима
-    :param c_frame_need: нужный n-ый кадр кажой секунды
+    Извлечение n кадров из каждой секунды видео. Кадры обрезаются в виде квадрата
+    :param video_path: путь к видео
+    :param timing_data: DataFrame с временными метками (T) и альфа (alpha)
+    :param n_frames_per_second: количество кадров, которое нужно извлечь из каждой секунды
     :return: None
     """
-
-    video_path = video_path_
-
-# Загрузка данных из файла с таймингом для 1го режима
-    #timing_data = pd.read_excel(timing_file)
-
-# Создаем директорию для хранения кадров, если её нет
-    output_dir = 'frames_by_modes'
+    #создаем папку для хранения
+    output_dir = 'frames_by_alpha'
     os.makedirs(output_dir, exist_ok=True)
 
-# Загрузка видео
+    #подгружаем видео
     cap = cv2.VideoCapture(video_path)
-    fps = cap.get(cv2.CAP_PROP_FPS) # 25
-    print(f'Проверить количество кадров из видео {fps}, соответствует реальности? {fps == 25}')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    if info:
+        print(f'Количество кадров в секунду (FPS): {fps}')
 
+    if not fps:
+        raise ValueError("FPS не определено ")
 
-    start_time = start_time_
-    end_time = end_time_
+    #интервал между кадрами в одной секунде
+    frame_interval = int(fps / n_frames_per_second)
 
-    # Переводим время в кадры
-    start_frame = time_to_frame(start_time, fps)
-    end_frame = time_to_frame(end_time, fps)
+    #группировка данные по альфе
+    grouped_data = timing_data.groupby('alpha_t')
 
-    # Папка для текущего режима
-    mode_dir = os.path.join(output_dir, f'mode_{mode_}')
-    os.makedirs(mode_dir, exist_ok=True)
+    for alpha, group in grouped_data:
+        #Создаем папку для текущей альфы
+        mode_dir = os.path.join(output_dir, f'mode_{alpha}')
+        os.makedirs(mode_dir, exist_ok=True)
+        TOTAL = 0
+        for _, row in group.iterrows():
+            start_time = row['T']
+            end_time = start_time + 1  #дергаем все кадры в пределах указанной секунды
 
-    # Переходим к стартовому кадру
-    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+            start_frame = time_to_frame(start_time, fps)
+            end_frame = time_to_frame(end_time, fps)
 
-    frame_count = start_frame
-    saved_frame_count = 0
+            #начинаем со стартового кадра
+            cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
-    # Чтение и сохранение кадров
-    while frame_count <= end_frame:
-        ret, frame = cap.read()
-        if not ret:
-            break
+            frame_count = start_frame
+            saved_frame_count = 0
 
-        next_sec = 0
-        if frame_count % c_frame_need == 0:  # Например, каждый 10-й кадр
-            ms = round(1 / fps, 2)
-            #print(ms)
-            # учесть что меняются секунды
-            frame_filename = os.path.join(mode_dir, f'{f'{start_time + next_sec + ms * c_frame_need * saved_frame_count}'}.jpg')
-            cv2.imwrite(frame_filename, frame)
+            while frame_count <= end_frame and saved_frame_count < n_frames_per_second:
+                ret, frame = cap.read()
+                if not ret:
+                    break
 
-            saved_frame_count += 1
+                #сохраняем кадр в папку
+                frame_filename = os.path.join(mode_dir, f'{start_time}_{saved_frame_count}.jpg')
+                #вырезаем квадрат
+                frame = frame[50:670, 355:975]
+                cv2.imwrite(frame_filename, frame)
 
-        frame_count += 1
+                saved_frame_count += 1
+                frame_count += frame_interval
+                TOTAL += 1
+        if info:
+            print(f'Подгруппа {alpha}, количество изображений {TOTAL} ')
 
     cap.release()
     print("Обработка завершена!")
-
-
-
-
-
